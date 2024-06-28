3128bb0b8a1ff961ef04cf5e393abe224a3889d0
Justin Marquez <justin.marquez@wisc.edu>
2024-06-28 08:58:22 -0500

    added bash scripts for creating certain EFT plots

---
+++ RunBash/plotZZ18_SMratio.sh
@@ -0,0 +1,24 @@
+#filename="Hists28May2024-ZZ4l2018_MVA.root" #Signal + Bgk + LHE + IDs (all coeffs + SMlimit + SM) eemm
+filename="Hists26Jun2024-ZZ4l2018_MVA.root" #Signal + Bgk + LHE + IDs (all coeffs + SMlimit + SM) eemm - 100K
+selection="ZZSelectionsTightLeps"
+VVAnalysis_path="/afs/hep.wisc.edu/user/marquez5/public/SMEFTsim/uwvv_analysis/histograms/src/Analysis/VVAnalysis"
+variable="Mass" # ZMass Z1Mass Z2Mass ZPt ZZPt LepPt LepEta"
+
+options="-s ZZ4l2018/$selection -l 59.7 -u stat --no_data --latex --hist_file $VVAnalysis_path/HistFiles/$filename --preliminary --scaleymax 1.2 --scalelegx 1.2 --lhe_weight_id 0.0"
+
+for restrict in cHWB cHG cll1; do
+  filelist="ZZEFT_${restrict}_SMlimit_nobkg"
+  dir="SMlimit-${restrict}ratio/eemm"
+  for var in $variable; do
+    moreoptions="-f $filelist --folder_name $dir -c eemm -b ${var}"
+    echo ${var}
+    if [ $var = "Mass" ]; then
+      echo "Plotting eemm channel"
+      ./makeHistStack.py $options $moreoptions --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0
+    else
+      echo "Attempting ${var}"
+      echo "Plotting eemm channel"
+      ./makeHistStack.py $options $moreoptions
+    fi
+  done
+done
