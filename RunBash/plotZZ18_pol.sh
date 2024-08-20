cd6083fbb191753b814b416aecc7db664933a75f
Justin Marquez <justin.marquez@wisc.edu>
2024-08-20 04:19:39 -0500

    Renamed plotZZ18.sh to reflect its focus on polarization

---
+++ RunBash/plotZZ18_pol.sh
@@ -0,0 +1,25 @@
+filename="Hists10Jul2024-ZZ4l2018_MVA.root" #Signal + Bgk + LHE + IDs (all coeffs + SMlimit + SM) eemm - 100K
+selection="ZZSelectionsTightLeps"
+VVAnalysis_path="/afs/hep.wisc.edu/user/marquez5/public/SMEFTsim/uwvv_analysis/histograms/src/Analysis/VVAnalysis"
+variable="Mass ZMass Z1Mass Z2Mass ZPt ZZPt LepPt LepEta Z1PolCos Z2PolCos"
+
+options="-s ZZ4l2018/$selection -l 59.7 -u stat --no_data --no_ratio --latex --hist_file $VVAnalysis_path/HistFiles/$filename --preliminary --scaleymax 1.2 --scalelegx 1.5 --lhe_weight_id 0.0"
+
+params="qqZ0Z0 qqZTZT"
+
+for param in $params; do
+  filelist="ZZEFT_${param}_nobkg"
+  dir="${param}/eemm"
+  for var in $variable; do
+    moreoptions="-f $filelist --folder_name $dir -c eemm -b ${var}"
+    echo ${var}
+    if [ $var = "Mass" ]; then
+      echo "Plotting eemm channel"
+      ./makeHistStack.py $options $moreoptions --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0
+    else
+      echo "Attempting ${var}"
+      echo "Plotting eemm channel"
+      ./makeHistStack.py $options $moreoptions
+    fi
+  done
+done
