7536d4dad890977248a654d64c23fd9038c124af
Justin Marquez <justin.marquez@wisc.edu>
2024-12-03 11:46:47 -0600

    modify for easier testing. rename options to opts for shorter lines

--- RunBash/plotZZ22.sh
+++ RunBash/plotZZ22.sh
@@ -3,31 +3,26 @@ analysis="ZZ4l2022"
 selection="ZZSelectionsTightLeps"
 VVAnalysis_path="$CMSSW_BASE/src/Analysis/VVAnalysis"
 variable="Mass ZMass Z1Mass Z2Mass LepPt LepEta Z1PolCos Z2PolCos"
+channels="eeee eemm mmmm"
 
-options="-s $analysis/$selection -l 37.85 -u stat --latex --hist_file $VVAnalysis_path/HistFiles/$filename --preliminary --scaleymax 1.2 --scalelegx 1.5"
+opts="-s $analysis/$selection -l 37.85 -u stat --latex --hist_file $VVAnalysis_path/HistFiles/$filename --preliminary --scaleymax 1.2 --scalelegx 1.5"
 
 filelist="$analysis"
 dir="output_$analysis"
 for var in $variable; do
-  moreoptions="-f $filelist -b ${var}"
+  moreopts="-f $filelist -b ${var}"
   echo ${var}
   if [ $var = "Mass" ]; then
-    echo "All Channels"
-    ./makeHistStack.py $options $moreoptions --folder_name $dir --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0
-    for ch in eeee eemm mmmm; do
-      echo "Plotting $ch channel"
-      ./makeHistStack.py $options $moreoptions --folder_name $dir/$ch -c $ch --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0
-    done
-    #echo "Plotting 2e2mu channel"
-    #./makeHistStack.py $options $moreoptions --folder_name $dir/2e2m -c eemm,mmee --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0
-  else
-    echo "All Channels"
-    ./makeHistStack.py $options $moreoptions --folder_name ${dir}
-    for ch in eeee eemm mmmm; do
-      echo "Plotting $ch channel"
-      ./makeHistStack.py $options $moreoptions --folder_name ${dir}/$ch -c $ch
-    done
-    #echo "Plotting 2e2mu channel"
-    #./makeHistStack.py $options $moreoptions --folder_name ${dir}/2e2m -c eemm,mmee
+    moreopts="$moreopts --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0"
+  #elif [ $var = "ZMass" ]; then
+  #  moreopts="$moreopts --rebin 0.0,20.0,40.0,60.0,80.0,100.0,120.0"
   fi
+  echo "All Channels"
+  ./makeHistStack.py $opts $moreopts --folder_name ${dir}
+  for ch in $channels; do
+    echo "Plotting $ch channel"
+    ./makeHistStack.py $opts $moreopts --folder_name ${dir}/$ch -c $ch
+  done
+  #echo "Plotting 2e2mu channel"
+  #./makeHistStack.py $opts $moreopts --folder_name ${dir}/2e2m -c eemm,mmee
 done
