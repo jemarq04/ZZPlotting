b41075bea0e4b0d26aea01dfbe0a4c1b6e956dbe
Justin Marquez <justin.marquez@wisc.edu>
2025-05-13 10:54:46 -0500

    move Sumw2 calls to be within the if check. comment them out for now - don't seem necessary

--- Utilities/FromFileLHEHistProducer.py
+++ Utilities/FromFileLHEHistProducer.py
@@ -71,16 +71,16 @@ class FromFileLHEHistProducer(HistProducer):
         
         if "Mass" in hist_name and "Full" in hist_name:
             #pdb.set_trace()
-            hist.Sumw2()
             normBW = True #Set False when printing table and don't want to normalize by BW
             if normBW:
+                #hist.Sumw2()
                 for ib in range(1,hist.GetNbinsX()+1):
                     width = hist.GetBinWidth(ib)
                     hist.SetBinContent(ib, hist.GetBinContent(ib)/width)
                     hist.SetBinError(ib, hist.GetBinError(ib)/ width)
                     if hist.GetBinError(ib) > hist.GetBinContent(ib):
                         hist.SetBinError(ib, hist.GetBinContent(ib))
-            hist.Sumw2() #This doesn't seem to overwrite error? What's its function?
+                #hist.Sumw2() #This doesn't seem to overwrite error? What's its function?
         
         return hist
 
