c41b578c9a0109d1c057176a18a9af002c112b57
Justin Marquez <justin.marquez@wisc.edu>
2025-05-16 10:14:16 -0500

    update hist file

--- RunBash/plotZZ23.sh
+++ RunBash/plotZZ23.sh
@@ -1,4 +1,4 @@
-filename="Hists14May2025-ZZ4l2023.root"
+filename="Hists16May2025-ZZ4l2023.root"
 analysis="ZZ4l2023"
 selection="ZZSelectionsTightLeps"
 VVAnalysis_path="$CMSSW_BASE/src/Analysis/VVAnalysis"
