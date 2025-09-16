7288a0cc5becadc69c730b131154b8cd64ad58e8
Justin Marquez <justin.marquez@wisc.edu>
2025-09-16 12:53:27 -0500

    rework script to plot both 2022 and 2023

--- RunBash/plotZZ.sh
+++ RunBash/plotZZ.sh
@@ -1,13 +1,30 @@
-filename="Hists12Sep2025-ZZ4l2022.root"
-analysis="ZZ4l2022"
+if [[ $# -ne 1 ]]; then
+  echo usage: $0 YEAR
+  exit 1
+elif [[ ! $1 =~ ^202[2-3]$ ]]; then
+  echo invalid year: $1
+  exit 1
+fi
+
+if [[ $1 = 2022 ]]; then lumi=34.652;
+elif [[ $1 = 2023 ]]; then lumi=27.76;
+fi
+
+analysis="ZZ4l$1"
+filename="Hists-ZZ4l$1.root"
+filepath="$VVAnalysis_path/HistFiles/$filename"
+if [[ ! -f $filepath ]]; then
+  echo invalid file: $filepath
+  exit 1
+fi
+
 selection="ZZSelectionsTightLeps"
 VVAnalysis_path="$CMSSW_BASE/src/Analysis/VVAnalysis"
 variables="Mass ZMass Z1Mass Z2Mass LepPt LepEta SIP3D LepIso nJets nJets_central Mass0jFull Mass1jFull Mass2jFull Mass34jFull absjetEta[0] absjetEta[1] jetPt[0] jetPt[1]"
 channels="eeee eemm mmee mmmm"
 dochannels=true
-lumi=34.652
 
-opts="-s $analysis/$selection -l $lumi -u stat --latex --hist_file $VVAnalysis_path/HistFiles/$filename --preliminary --scaleymax 1.2 --scalelegx 1.2"
+opts="-s $analysis/$selection -l $lumi -u stat --latex --hist_file $filepath --preliminary --scaleymax 1.2 --scalelegx 1.2"
 
 dir="output"
 filelist="$analysis"
