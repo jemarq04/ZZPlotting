123140d87b9a32dbb9c168066c59e4c6d47fbf8e
Justin Marquez <justin.marquez@wisc.edu>
2025-10-20 11:21:42 -0500

    add leading and subleading lepton pt

--- RunBash/plotZZ.sh
+++ RunBash/plotZZ.sh
@@ -22,7 +22,7 @@ if [[ ! -f $filepath ]]; then
 fi
 
 selection="ZZSelectionsTightLeps"
-variables="Mass ZMass Z1Mass Z2Mass LepPt LepEta SIP3D LepIso" 
+variables="Mass ZMass Z1Mass Z2Mass LepPt LepPt1 LepPt2 LepEta SIP3D LepIso"
 jetvariables="nJets nJets_central MassFull Mass0jFull Mass1jFull Mass2jFull Mass34jFull absjetEta[0] absjetEta[1] jetPt[0] jetPt[1]"
 channels="eeee eemm mmee mmmm"
 dochannels=true
