a9f4aef60443503c0aa6091a5e171f906c5ef463
Justin Marquez <justin.marquez@wisc.edu>
2025-10-31 10:26:44 -0500

    add commented line for 2022-2024 lumi

--- RunBash/plotZZ.sh
+++ RunBash/plotZZ.sh
@@ -8,7 +8,8 @@ fi
 
 if [[ $1 = 2022 ]]; then lumi=34.652;
 elif [[ $1 = 2023 ]]; then lumi=27.76;
-elif [[ $1 = Run3Combined ]]; then lumi=62.412;
+elif [[ $1 = Run3Combined ]]; then lumi=62.412
+#elif [[ $1 = Run3Combined ]]; then lumi=171.342;
 elif [[ $1 = 2024 ]]; then lumi=109.33;
 fi
 
