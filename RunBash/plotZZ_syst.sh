b3e1ce86bb959bec11960616a64abb8684dc34ad
Justin Marquez <justin.marquez@wisc.edu>
2026-05-19 14:41:59 -0500

    feat: update systematic variation plotting script to more closely match usage with other scripts in the framework. add automatic histogram titles

--- RunBash/plotZZ_syst.sh
+++ RunBash/plotZZ_syst.sh
@@ -14,6 +14,19 @@ if [[ ! -f $filepath ]]; then
   exit 1
 fi
 
+selection="SystematicVariations"
+variables="Mass"
 systematics="CMS_eff_e,CMS_eff_m,CMS_pileup"
+channels="eeee eemm mmee mmmm"
+dir="output"
 
-./plotVariations.py --title ";m_{4l} [GeV];Events" --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0 -S $systematics -a ZZ4l$1 -y $1 --preliminary $filepath
+opts="-s $analysis/$selection -y $1 --hist_file $filepath --preliminary --scaleymax 1.2 -S $systematics"
+
+for var in $variables; do
+  echo ${var}
+
+  moreopts="--folder_name ${dir} -b ${var}"
+  [[ $var = "Mass" ]] && moreopts="$moreopts --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0"
+
+  ./plotVariations.py $opts $moreopts
+done
--- plotVariations.py
+++ plotVariations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import argparse
 import array
 import configparser
 import json
 import os
 import sys
 import ROOT
+from Utilities.ConfigHistFactory import ConfigHistFactory
+import Utilities.UserInput as UserInput
 import Utilities.helper_functions as helper
 import Utilities.plot_functions as plotter
 from Utilities.scripts import makeSimpleHtml
@@ -16,6 +17,28 @@ with open("Templates/config.%s" % os.getlogin()) as fconfig:
     sys.path.insert(0, config["Setup"]["scriptPath"].replace("$CMSSW_BASE", os.environ["CMSSW_BASE"]))
 import ConfigureJobs
 import HistTools
+
+
+def getComLineArgs():
+    parser = UserInput.getDefaultParser()
+    parser.add_argument("-s", "--selection", type=str, required=True, help="Specificy selection level to run over")
+    parser.add_argument(
+        "-b",
+        "--branches",
+        type=lambda val: [x.strip() for x in val.split(",")],
+        default=["Mass"],
+        help="List (separate by commas) of names of branches in root and config file to plot",
+    )
+    parser.add_argument("-g", "--group", default="qqZZ-powheg", help="name of plot group to draw with variations")
+    parser.add_argument(
+        "-S",
+        "--systematics",
+        required=True,
+        type=lambda val: [x.strip() for x in val.split(",")],
+        help="name of systematic systematic to draw",
+    )
+    parser.add_argument("-y", "--year", required=True, help="year for analysis")
+    return parser.parse_args()
 
 
 def savePlotWithRatio(oldcanvas, dimensions, ratio_text, ratio_range, plot_path, html_path, plot_name, args):
@@ -122,38 +145,15 @@ def savePlotWithRatio(oldcanvas, dimensions, ratio_text, ratio_range, plot_path,
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-l", "--luminosity", type=float, help="Luminosity in fb-1")
-    parser.add_argument("-s", "--sample", default="qqZZ-powheg", help="name of plot group to draw with variations")
-    parser.add_argument(
-        "--rebin",
-        type=lambda val: [float(x) for x in val.split(",")],
-        default=[],
-        help="list of comma-separated floats for hist rebinning",
-    )
-    parser.add_argument("-v", "--variable", default="Mass", help="name of variable to plot")
-    parser.add_argument(
-        "-S",
-        "--systematics",
-        required=True,
-        type=lambda val: [x.strip() for x in val.split(",")],
-        help="name of systematic systematic to draw",
-    )
-    parser.add_argument("-a", "--analysis", required=True, help="name of analysis (e.g. ZZ4l2022)")
-    parser.add_argument("-y", "--year", required=True, help="year of analysis")
-    parser.add_argument("--title", default="", help="title to set for histogram before plotting")
-    parser.add_argument("--thesis", action="store_true", help="Write 'Thesis' in CMS style text")
-    parser.add_argument("--preliminary", action="store_true", help="Write 'Preliminary' in CMS style text")
-    parser.add_argument("--simulation", action="store_true", help="Write 'Simulation' in CMS style text")
-    parser.add_argument("--folder_name", type=str, default="output", help="folder name to save plots in")
-    parser.add_argument("infile", help="input histogram file")
-    args = parser.parse_args()
+    args = getComLineArgs()
 
-    print(f"{args.analysis}{args.systematics}: Using input file {args.infile}")
+    print(f"{args.selection}: {args.systematics}")
+    print(f"Using input file {args.hist_file}")
 
     manager_path = ConfigureJobs.getManagerPath()
+    manager_name = ConfigureJobs.getManagerName()
 
-    if args.luminosity is None:
+    if args.luminosity == -1:
         args.luminosity = ConfigureJobs.getLuminosity(args.year, manager_path=manager_path)
 
     lumi_text = []
@@ -168,129 +168,137 @@ def main():
     args.output_file = ""
     args.no_html = False
 
-    samples = [args.sample]
-    with open(
-        os.path.join(manager_path, ConfigureJobs.getManagerName(), "PlotGroups", f"{args.analysis}.json")
-    ) as infile:
+    samples = [args.group]
+    with open(os.path.join(manager_path, manager_name, "PlotGroups", f"{args.selection.split('/')[0]}.json")) as infile:
         info = json.load(infile)
-        if args.sample in info:
-            samples = info[args.sample]["Members"]
+        if args.group in info:
+            samples = info[args.group]["Members"]
 
     config_name = "Templates/config.%s" % os.getlogin()
-    if not os.path.isfile(config_name):
-        parser.error("Failed to find valid config file. Looking for %s" % config_name)
     config = configparser.ConfigParser()
     with open(config_name) as fconfig:
         config.read_file(fconfig)
-    if "gituser" not in config["Setup"]:
-        parser.error("gituser not specified in config file %s" % config_name)
     ROOT.dotrootImport("%s/CMSPlotDecorations" % config["Setup"]["gituser"])
+
+    try:
+        config_factory = ConfigHistFactory(f"{manager_path}/{manager_name}", args.selection.split("_")[0])
+    except FileNotFoundError:
+        config_factory = ConfigHistFactory(
+            f"{manager_path}/{manager_name}", f"{args.selection.split('/')[0]}/ZZSelectionsTightLeps"
+        )
 
     ROOT.gROOT.SetBatch(True)
     ROOT.gStyle.SetLegendBorderSize(0)
     ROOT.gStyle.SetOptStat(0)
     ROOT.gStyle.SetLineWidth(3)
 
-    with ROOT.TFile.Open(args.infile) as infile:
-        for systematic in args.systematics:
-            channels = ["eeee", "eemm", "mmee", "mmmm"]
-            plotnames = ["_".join([args.variable, chan]) for chan in channels]
-            plotnames += [
-                "_".join([args.variable, systematic + var, chan]) for var in ["Up", "Down"] for chan in channels
-            ]
+    with ROOT.TFile.Open(args.hist_file) as infile:
+        for branch in args.branches:
+            for systematic in args.systematics:
+                channels = ["eeee", "eemm", "mmee", "mmmm"]
+                plotnames = ["_".join([branch, chan]) for chan in channels]
+                plotnames += ["_".join([branch, systematic + var, chan]) for var in ["Up", "Down"] for chan in channels]
 
-            group, _ = HistTools.makeCompositeHists(
-                infile,
-                args.sample,
-                ConfigureJobs.getListOfFilesWithXSec(samples, manager_path),
-                args.luminosity,
-                hists=plotnames,
-                rebin=array.array("d", args.rebin) if args.rebin else None,
-            )
-
-            for histname in plotnames:
-                if not histname.endswith(channels[0]):
-                    continue
-                basename = "_".join(histname.split("_")[:-1])
-                hsum = group.FindObject(histname).Clone(f"{basename}_all")
-                for chan in channels[1:]:
-                    hsum.Add(group.FindObject(f"{basename}_{chan}"))
-                group.Add(hsum)
-
-                hsum_2e2m = group.FindObject(f"{basename}_eemm").Clone(f"{basename}_2e2m")
-                hsum_2e2m.Add(group.FindObject(f"{basename}_mmee"))
-                group.Add(hsum_2e2m)
-
-            channels += ["2e2m", "all"]
-            plot_name = f"{args.variable}_{systematic}"
-            for chan in channels:
-                print(f"Plotting {chan}...")
-                folder_name = f"{args.folder_name}/{chan}" if chan != "all" else args.folder_name
-                plot_path, html_path = helper.getPlotPaths(f"{args.analysis}/SystematicVariations", folder_name, False)
-
-                hist = group.FindObject("_".join([args.variable, chan]))
-                histUp = group.FindObject("_".join([args.variable, systematic + "Up", chan]))
-                histDown = group.FindObject("_".join([args.variable, systematic + "Down", chan]))
-
-                histUp.SetLineStyle(2)
-                histDown.SetLineStyle(2)
-
-                if args.title is not None:
-                    hist.SetTitle(args.title)
-
-                canvasDimensions = [800, 800]
-                canvas = ROOT.TCanvas(f"canvas_{chan}", "canvas", *canvasDimensions)
-
-                # legend coordinates
-                offset = ROOT.gPad.GetRightMargin() - 0.04
-                width = 0.2
-                xdist = 0.91
-                xcoords = [xdist - width - offset, xdist - offset]
-                ymax = 0.8
-                ycoords = [ymax, ymax - 0.16]
-                coords = [xcoords[0], ycoords[0], xcoords[1], ycoords[1]]
-
-                stack = ROOT.THStack(f"stack_{plot_name}_{chan}", args.title.split(";")[0])
-                stack.Add(hist)
-                stack.Add(histUp)
-                stack.Add(histDown)
-                stack.Draw("nostack hist")
-
-                stack.GetYaxis().SetTitleSize(hist.GetYaxis().GetTitleSize())
-                stack.GetYaxis().SetTitleOffset(hist.GetYaxis().GetTitleOffset())
-                stack.GetYaxis().SetTitle(hist.GetYaxis().GetTitle())
-                if hist.GetMinimum() == 0.0:
-                    stack.GetYaxis().ChangeLabel(1, -1.0, 0)
-                stack.GetHistogram().GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
-                stack.GetHistogram().SetLabelSize(0.04)
-                stack.SetMinimum(hist.GetMinimum())
-                stack.SetMaximum(max([x.GetMaximum() for x in stack.GetHists()]) * 1.2)
-                stack.GetHistogram().GetYaxis().SetTitleOffset(1.05)
-
-                legend = ROOT.TLegend(*coords)
-                legend.AddEntry(hist, args.sample, "l")
-                legend.AddEntry(histUp, systematic, "l")
-                legend.Draw()
-                ROOT.CMSlumi(canvas, 0, 0, f"{args.luminosity:.1f} fb^{{-1}} (13.6 TeV)", " ".join(lumi_text))
-
-                canvas = savePlotWithRatio(
-                    canvas,
-                    canvasDimensions,
-                    "#scale[0.85]{syst. / cent.}",
-                    [0.9, 1.1],
-                    plot_path,
-                    html_path,
-                    plot_name,
-                    args,
+                group, _ = HistTools.makeCompositeHists(
+                    infile,
+                    args.group,
+                    ConfigureJobs.getListOfFilesWithXSec(samples, manager_path),
+                    args.luminosity,
+                    hists=plotnames,
+                    rebin=array.array("d", args.rebin) if args.rebin else None,
                 )
 
-                makeSimpleHtml.writeHTML(
-                    html_path.replace("/plots", ""), f"Systematic Variations on {args.sample}", latest=plot_name
-                )
+                for histname in plotnames:
+                    if not histname.endswith(channels[0]):
+                        continue
+                    basename = "_".join(histname.split("_")[:-1])
+                    hsum = group.FindObject(histname).Clone(f"{basename}_all")
+                    for chan in channels[1:]:
+                        hsum.Add(group.FindObject(f"{basename}_{chan}"))
+                    group.Add(hsum)
 
-                canvas.Close()
+                    hsum_2e2m = group.FindObject(f"{basename}_eemm").Clone(f"{basename}_2e2m")
+                    hsum_2e2m.Add(group.FindObject(f"{basename}_mmee"))
+                    group.Add(hsum_2e2m)
 
-            del group
+                channels += ["2e2m", "all"]
+                plot_name = f"{branch}_{systematic}"
+                for chan in channels:
+                    print(f"Plotting {chan}...")
+                    folder_name = f"{args.folder_name}/{chan}" if chan != "all" else args.folder_name
+                    plot_path, html_path = helper.getPlotPaths(args.selection, folder_name, False)
+
+                    hist = group.FindObject("_".join([branch, chan]))
+                    histUp = group.FindObject("_".join([branch, systematic + "Up", chan]))
+                    histDown = group.FindObject("_".join([branch, systematic + "Down", chan]))
+
+                    config_factory.setHistAttributes(hist, branch, args.group)
+                    config_factory.setHistAttributes(histUp, branch, args.group)
+                    config_factory.setHistAttributes(histDown, branch, args.group)
+                    hist.SetFillColor(0)
+                    histUp.SetFillColor(0)
+                    histDown.SetFillColor(0)
+
+                    hist.SetLineStyle(1)
+                    histUp.SetLineStyle(5)
+                    histDown.SetLineStyle(5)
+                    hist.SetLineWidth(2)
+                    histUp.SetLineWidth(2)
+                    histDown.SetLineWidth(2)
+
+                    canvasDimensions = [800, 800]
+                    canvas = ROOT.TCanvas(f"canvas_{chan}", "canvas", *canvasDimensions)
+
+                    # legend coordinates
+                    offset = ROOT.gPad.GetRightMargin() - 0.04
+                    width = 0.2 * args.scalelegx
+                    xdist = 0.91
+                    xcoords = [xdist - width - offset, xdist - offset]
+                    ymax = 0.8
+                    ycoords = [ymax, ymax - 0.16 * args.scalelegy]
+                    coords = [xcoords[0], ycoords[0], xcoords[1], ycoords[1]]
+
+                    stack = ROOT.THStack(f"stack_{plot_name}_{chan}", "")
+                    stack.Add(hist)
+                    stack.Add(histUp)
+                    stack.Add(histDown)
+                    stack.Draw("nostack hist")
+
+                    stack.GetYaxis().SetTitleSize(hist.GetYaxis().GetTitleSize())
+                    stack.GetYaxis().SetTitleOffset(hist.GetYaxis().GetTitleOffset())
+                    stack.GetYaxis().SetTitle(hist.GetYaxis().GetTitle())
+                    if hist.GetMinimum() == 0.0:
+                        stack.GetYaxis().ChangeLabel(1, -1.0, 0)
+                    stack.GetHistogram().GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
+                    stack.GetHistogram().SetLabelSize(0.04)
+                    stack.SetMinimum(hist.GetMinimum() * args.scaleymin)
+                    stack.SetMaximum(max([x.GetMaximum() for x in stack.GetHists()]) * args.scaleymax)
+                    stack.GetHistogram().GetYaxis().SetTitleOffset(1.05)
+
+                    legend = ROOT.TLegend(*coords)
+                    legend.AddEntry(hist, args.group, "l")
+                    legend.AddEntry(histUp, systematic, "l")
+                    legend.Draw()
+                    ROOT.CMSlumi(canvas, 0, 0, f"{args.luminosity:.1f} fb^{{-1}} (13.6 TeV)", " ".join(lumi_text))
+
+                    canvas = savePlotWithRatio(
+                        canvas,
+                        canvasDimensions,
+                        "#scale[0.85]{syst. / cent.}",
+                        [0.9, 1.1],
+                        plot_path,
+                        html_path,
+                        plot_name,
+                        args,
+                    )
+
+                    makeSimpleHtml.writeHTML(
+                        html_path.replace("/plots", ""), f"Systematic Variations on {args.group}", latest=plot_name
+                    )
+
+                    canvas.Close()
+
+                del group
 
     print("Done.")
 
