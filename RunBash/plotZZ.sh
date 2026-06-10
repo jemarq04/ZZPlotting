8d58a8b724212a8c39f862fc1fc223636b900d56
Justin Marquez <justin.marquez@wisc.edu>
2026-06-10 09:07:34 -0500

    feat: add scaleymax option to lepton phi plots and for eta,phi plots in CR

--- RunBash/plotZZ.sh
+++ RunBash/plotZZ.sh
@@ -32,7 +32,7 @@ for var in $variables; do
   moreopts="-f $filelist -b ${var}"
   [[ $var = "Mass" ]] && moreopts="$moreopts --rebin 100.0,200.0,250.0,300.0,350.0,400.0,500.0,600.0,800.0,1000.0"
   [[ $var =~ Z[12]?Mass ]] && moreopts="$moreopts --legend_left"
-  [[ $var =~ ^(Lep|Mu|Ele)Eta$ ]] && moreopts="$moreopts --scaleymax 2.0"
+  [[ $var =~ ^(Lep|Mu|Ele)(Eta|Phi)$ ]] && moreopts="$moreopts --scaleymax 2.0"
 
   echo "All Channels"
   ./makeHistStack.py $opts $moreopts --folder_name ${dir}
@@ -74,6 +74,7 @@ for var in $controlvariables; do
 
     moreopts="-f ${filelist}_nonprompt -b ${var}_${fakes}"
     [[ $var =~ Z[12]?Mass ]] && moreopts="$moreopts --legend_left"
+    [[ $var =~ (Lep|Mu|Ele)(Eta|Phi)$ ]] && moreopts="$moreopts --scaleymax 2.1"
 
     echo "All Channels"
     ./makeHistStack.py $opts $moreopts --folder_name ${dir} --extra_text "${extratext}"
